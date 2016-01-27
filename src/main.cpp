b0855553c91453be43606f6a562d466d22eda31d
Alexander Oleinik <alxndr@solo.vrmnet>
2016-01-27 13:36:34 -0500

    Continuing work on main, added sphere submodule

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
  *
  *       Filename:  main.cpp
  *
- *    Description:  
+ *    Description:  There are 3 States in this process.
+ *    ------------------ 1 ---------------------------
+ *    In the first state, the bubble is free to grow and move. IE 
  *
  *        Version:  1.0
  *        Created:  01/27/2016 10:52:28 AM
@@ -19,13 +21,57 @@
 #include <iostream>
 #include <string>
 
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  stage1(bubble *s, int i)
+ *  Description:  Grows a sphere not in contact with any other bubble, until
+ *  a collision
+ * =====================================================================================
+ */
+sphere stage1(bubble *s, int i)
+{
+    
+}
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  read_sphere_file
+ *  Description:  Reads sphere coordinates from file
+ * =====================================================================================
+ */
+int read_sphere_coords(){
+    std::ifstream file("spheres.list");
+    int count = 0;
+    double pore_volume = X_MAX+Y_MAX+Z_MAX - sphere_volume;
+    if(file.is_open())
+    {
+        std::string line;
+        while(getline(file, line))
+        {
+            std::istringstream iss(line);
+            double d;
+            double params[5];
+            int paramcount=0;;
+            while (iss>>d){
+                params[paramcount]=d;
+                paramcount++;
+            }
+            spheres[count]=(sphere){params[1], (vec3){params[2],params[3],params[4]}};
+            sphere_volume+=(4.0/3.0)*PI*pow(spheres[count].radius,3);
+            count++;
+        }
+    }
+    return count;
+}
+
 /* 
  * ===  FUNCTION  ======================================================================
  *         Name:  main
  *  Description:  main loop
  * =====================================================================================
  */
-
 int main(int argc, char * argv[])
 {
     if(argc!=3)
